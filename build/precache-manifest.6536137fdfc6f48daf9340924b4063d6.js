f998a49e4efa5729091825e4082dcd1b1c1bc8f4
Arushi <aruruthala2001@gmail.com>
2023-08-29 00:13:08 +0530

    initial commit

---
+++ build/precache-manifest.6536137fdfc6f48daf9340924b4063d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb7f36b9e7000341e47c1a5eab38385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "623016d2dd32c5cb0811",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623016d2dd32c5cb0811",
+    "url": "/static/js/main.64ffb267.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
